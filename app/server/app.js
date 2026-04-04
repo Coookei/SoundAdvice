0df52cc4b8331b89b2ea3219e023bfbcac3a0aef
Coookei <89310401+Coookei@users.noreply.github.com>
2026-04-04 21:46:03 +0100

    feat: restrict direct access to html files so protected routing is enforced

--- app/server/app.js
+++ app/server/app.js
@@ -11,13 +11,14 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(sessionMiddleware);
-app.use(express.static(join(__dirname, '../public'))); // serve ALL static assets from public dir
+app.use(sessionMiddleware); // attach req.userId from session cookie on every request
+app.use('/html', (_req, res) => res.status(403).send('Forbidden')); // block direct access to html files, only served via protected page routes
+app.use(express.static(join(__dirname, '../public'))); // serve ALL static assets from public dir, while protecting from traversing out in url eg ../../
 
 // page routes and API routes
 app.use('/', indexRouter);
 
-// not found page
+// not found 404 page
 app.use((_req, res) => res.status(404).send('404 - Page not found'));
 
 export default app;
