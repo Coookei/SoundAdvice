f7a7a509e00a1f9e7bb89d2bfdb5411295173819
Coookei <89310401+Coookei@users.noreply.github.com>
2026-04-09 20:34:09 +0100

    feat: implement comments functionality on posts with authenticated and ratelimited API routes, comments table, controllers and queries

--- app/server/routes/posts.js
+++ app/server/routes/posts.js
@@ -10,10 +10,13 @@ import {
   updatePostStatus,
   searchPosts,
 } from '../controllers/posts.js';
+import { getComments, createComment, deleteComment } from '../controllers/comments.js';
 import { requireAdmin, requireAuth } from '../middleware/auth.api.js';
 import { rateLimit } from '../middleware/rate_limit.js';
 
 const router = Router();
+
+// GET /my, /search and /admin routes need be before /:id to avoid conflicts
 
 router.get('/', getPosts); // get all approved public posts
 router.get('/search', searchPosts); // search approved posts with a query
@@ -33,6 +36,21 @@ router.delete(
   deletePost
 ); // authd users can delete their own posts, admin can delete any post
 
-// GET /my, /search and /admin routes need be before /:id to avoid conflicts
+router.get('/:id/comments', getComments); // get all comments for a post, approved posts comments are public, unapproved posts comments only visible to author or admin
+// can create 15 comments every 10 mins
+router.post(
+  '/:id/comments',
+  requireAuth,
+  rateLimit({ max: 15, windowMs: 10 * 60 * 1000, blockMs: 15 * 60 * 1000 }),
+  createComment
+); // authd users can create comments on approved posts
+
+// can delete 20 comments every 10 mins
+router.delete(
+  '/:id/comments/:commentId',
+  requireAuth,
+  rateLimit({ max: 20, windowMs: 10 * 60 * 1000, blockMs: 10 * 60 * 1000 }),
+  deleteComment
+); // authd users can delete their own comments, admin can delete any comment
 
 export default router;
