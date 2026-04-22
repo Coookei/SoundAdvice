eee7cf443ac9ade8ad08bd8bf148801b3366041c
OllyM7 <olly.mayes@yahoo.com>
2026-04-22 15:40:26 +0100

    fix: move uploads static route before 404 handler

--- app/server/app.js
+++ app/server/app.js
@@ -17,14 +17,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(sessionMiddleware); // attach req.userId from session cookie on every request
 app.use('/html', (_req, res) => res.status(403).send('Forbidden')); // block direct access to html files, only served via protected page routes
 app.use(express.static(join(__dirname, '../public'))); // serve ALL static assets from public dir, while protecting from traversing out in url eg ../../
+app.use('/uploads', express.static(path.resolve('uploads'))); // serve uploaded profile pictures
 
 // page routes and API routes
 app.use('/', indexRouter);
 
 // not found 404 page
 app.use((_req, res) => res.status(404).send('404 - Page not found'));
-
-// pfp upload
-app.use('/uploads', express.static('uploads')); 
 
 export default app;
--- app/server/index.js
+++ app/server/index.js
@@ -1,12 +1,8 @@
-import './config.js'; 
+import './config.js';
 import app from './app.js';
-import express from 'express';
-import path from 'path'; 
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-app.use('/uploads', express.static(path.resolve('uploads'))); 
